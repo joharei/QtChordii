6d05ca285000a78cc14c2f1c8b1aa4b0f56806e2
Johan Reitan <johan.reitan@gmail.com>
2013-01-28 19:58:34 +0100

    Use Chordii to generate ps output.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #coding: utf-8
 import argparse
 import os
+import subprocess
 import sys
 from PySide import QtCore, QtGui
 from PySide.QtCore import QDir, QModelIndex
@@ -181,10 +182,16 @@ class MainForm(QtGui.QMainWindow):
         saveFileAct.triggered.connect(self.saveFile)
         fileMenu.addAction(saveFileAct)
 
+        fileMenu.addSeparator()
+
         saveProjectFileAct = QtGui.QAction(self.tr("Save &project"), self)
 #        saveProjectFileAct.setShortcut(QtGui.QKeySequence.Save)
         saveProjectFileAct.triggered.connect(self.saveProject)
         fileMenu.addAction(saveProjectFileAct)
+
+        exportFileAct = QtGui.QAction(self.tr("&Export songbook to PostScript..."), self)
+        exportFileAct.triggered.connect(self.runChordii)
+        fileMenu.addAction(exportFileAct)
 
         fileMenu.addSeparator()
 
@@ -208,6 +215,34 @@ class MainForm(QtGui.QMainWindow):
             os.makedirs(outDir)
         saveFile = open(outDir + "songbook.cho", "w")
         saveFile.write(saveString)
+
+    def runChordii(self, inputFile = None, outputFile = None):
+        """Run Chordii to produce output"""
+        command = ["chordii", "-i", "-L"]
+        if not any((inputFile, outputFile)):
+            outputFile = self.workingDir + "output/songbook.ps"
+            outDir = self.workingDir + "output/"
+            if not os.path.exists(outDir):
+                os.makedirs(outDir)
+            parent = self.model.index(self.workingDir)
+            for i in range(self.model.rowCount(parent)):
+                row = self.model.index(i,0,parent)
+                path = self.model.fileInfo(row).absoluteFilePath()
+                command.append(path)
+        command.append("-o")
+        command.append(outputFile)
+        try:
+            response = subprocess.check_output(command,
+                stderr=subprocess.STDOUT)
+            if response is not None:
+                msgBox = QMessageBox()
+                msgBox.setWindowTitle(self.tr(self.appName + " - Chordii warning"))
+                msgBox.setText(self.tr("Chordii exited with warnings."))
+                msgBox.setDetailedText(self.tr(bytearray(response).decode()))
+                msgBox.setIcon(QMessageBox.Warning)
+                msgBox.exec_()
+        except subprocess.CalledProcessError as e:
+            QMessageBox.critical(self, self.tr(self.appName + " - Chordii problem"), self.tr(e.output))
 
 if __name__ == "__main__":
     app = QtGui.QApplication(sys.argv)
